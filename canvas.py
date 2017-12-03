680ef193e901a631502b8486b18b4397dd45ab06
Marcelo Drudi <mdrudi@gmail.com>
2017-12-03 16:53:51 -0200

    Adding method to set a single pixel in the canvas

--- canvas.py
+++ canvas.py
@@ -9,3 +9,7 @@ class Canvas(object):
     def __init__(self, columns, rows):
         """Initialize the Matrix (Canvas)"""
         self.canvas = [['O' for j in range(columns)] for i in range(rows)]
+
+    def set_pixel(self, column, row, color):
+        """Set a singles pixel in the canvas"""
+        self.canvas[row - 1][column - 1] = color
--- test_canvas.py
+++ test_canvas.py
@@ -16,9 +16,24 @@ class TestCanvas(unittest.TestCase):
                 ['O', 'O', 'O', 'O', 'O'],
             ]
 
+        cls.pixel_set = [
+                ['O', 'O', 'O', 'O', 'O'],
+                ['O', 'O', 'O', 'O', 'O'],
+                ['O', 'O', 'X', 'O', 'O'],
+                ['O', 'O', 'O', 'O', 'O'],
+                ['O', 'O', 'O', 'O', 'O'],
+                ['O', 'O', 'O', 'O', 'O'],
+            ]
+
+
     def test_create_new_canvas(self):
         canvas = Canvas(5, 6)
         self.assertEqual(self.new_canvas, canvas.canvas)
+
+    def test_set_pixel_on_3_3(self):
+        canvas = Canvas(5, 6)
+        canvas.set_pixel(3, 3, 'X')
+        self.assertEqual(self.pixel_set, canvas.canvas)
 
 if __name__ == '__main__':
     unittest.main()
